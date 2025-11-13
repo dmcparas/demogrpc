c11958ad6e85d8d6563b9134640cc9ec3ac2453b
divyaprakash-tf <divya.prakash@candescent.com>
2025-11-14 02:54:46 +0530

    Add changes for kafka

--- startup.sh
+++ startup.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
-# wait for Zookeeper
-echo "Waiting for Zookeeper..."
+#!/bin/sh
+# Wait for Zookeeper to be ready
+echo "Waiting for Zookeeper on localhost:2181..."
 while ! nc -z localhost 2181; do
   sleep 1
 done
 echo "Zookeeper is up, starting Kafka..."
+
+# Start Kafka
 exec /etc/confluent/docker/run
