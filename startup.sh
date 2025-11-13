520aebbbff0e031f26eab4b04fc39c56b663cc62
divyaprakash-tf <divya.prakash@candescent.com>
2025-11-14 03:13:19 +0530

    Add changes for kafka

--- startup.sh
+++ startup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# Wait for Zookeeper (in the same container, we can start it directly)
+# Start Zookeeper in background
 echo "Starting Zookeeper..."
-/etc/confluent/docker/run &   # Starts Zookeeper in background
+zookeeper-server-start /etc/kafka/zookeeper.properties &
 
-# Wait for Zookeeper to be ready
+# Wait until Zookeeper is ready
 while ! nc -z localhost 2181; do
   sleep 1
 done
 
 echo "Starting Kafka..."
-exec /etc/confluent/docker/run  # Starts Kafka
+exec /etc/confluent/docker/run
